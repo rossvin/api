9531ec43f12af7d00b778c062aba3226897d2f51
Sergey Galaktionov <wgss@bigmir.net>
2019-04-14 19:48:52 +0300

    '123'

--- server/config.php
+++ server/config.php
@@ -1,11 +1,7 @@
 <?php
 // DB
-//define ("HOST", "127.0.0.1:3308");
-//define ("DBNAME", "secret1");
-//define ("USER", "root");
-//define ("PASS", "");
+define ("HOST", "127.0.0.1:3308");
+define ("DBNAME", "secret1");
+define ("USER", "root");
+define ("PASS", "");
 
-define ("HOST", "localhost");
-define ("DBNAME", "ykslfovc_mydb");
-define ("USER", "ykslfovc");
-define ("PASS", "c)g7De32@LH7Pm");
